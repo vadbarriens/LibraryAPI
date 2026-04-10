baedbdabf635a98d0e7273d5120d26e0d68199ff
vadbarriens <burinvad@icloud.com>
2026-04-10 12:53:47 +0300

    redact settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
